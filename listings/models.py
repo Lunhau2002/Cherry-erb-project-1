1dc4ba6bfb54cdc483850599c32801e582087988
Cherry <chlun1214@gmail.com>
2025-03-05 12:29:41 +0800

    Step 5 : admin

--- listings/models.py
+++ listings/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor 
 
-# Create your models here.
-class Listings(models.Model):
+class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
@@ -17,6 +16,8 @@ class Listings(models.Model):
     sqft = models.IntegerField()
     estate_size = models.FloatField(default=0.0)
     is_published = models.BooleanField(default=True)
+    # list_date = models.DateTimeField(auto_now_add=True, blank=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
@@ -25,5 +26,8 @@ class Listings(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
 
-def __str__(self):
-    return f"House Title : {self.title}"
+    def __str__(self):
+        return f"House Title: {self.title}"
+
+
+
