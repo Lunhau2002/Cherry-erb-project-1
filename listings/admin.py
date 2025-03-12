6ca9de2c90d4faf43cc4c9dcd8708fe0f58376af
Cherry <chlun1214@gmail.com>
2025-03-12 14:16:56 +0800

    Step 9 : search

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import NumberInput
+from django.db import models
 
 # Register your models here.
 
@@ -10,7 +12,10 @@ class ListingAdmin(admin.ModelAdmin):
     list_editable = 'is_published', 'price'
     search_fields = 'title', 'description', 'address', 'price'
     list_per_page = 25
-    ordering = ['-id']
+    ordering = ['-id']    
+    formfield_overrides = {
+        models.IntegerField: {'widget':NumberInput(attrs={'size' : '10'})}
+    }
 
 admin.site.register(Listing, ListingAdmin)
 
